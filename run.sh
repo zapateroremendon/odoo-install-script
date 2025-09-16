bfa5ed1cf10850887962ea9f0e13b468244da472
Isidoro Roa <37345629+zapateroremendon@users.noreply.github.com>
2025-09-16 14:09:37 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ################################################################################
-# Script for installing Odoo on Ubuntu 14.04, 15.04 and 16.04 (could be used for other version too)
+# Script for installing Odoo on Ubuntu 24.04 (could be used for other version too)
 # Author: Yenthe Van Ginneken
 #-------------------------------------------------------------------------------
-# This script will install Odoo on your Ubuntu 14.04 server. It can install multiple Odoo instances
+# This script will install Odoo on your Ubuntu 24.04 server. It can install multiple Odoo instances
 # in one Ubuntu because of the different xmlrpc_ports
 #-------------------------------------------------------------------------------
 # Make a new file:
